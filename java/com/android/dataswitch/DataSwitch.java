3d2c88df6cb5bbe7540a5f4e18850fbcfcdd2adc
Ujjwal Mishra <ujjwalmishra09@gmail.com>
2016-06-12 13:55:04 +0530

    First Commit

---
+++ java/com/android/dataswitch/DataSwitch.java
@@ -0,0 +1,101 @@
+package com.android.dataswitch;
+
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by ujjwalmishra on 6/12/16.
+ */
+public class DataSwitch  extends Thread {
+    private ServerSocket serverSocket;
+    private int portNumber;
+    private ConnectivityManager connectivityManager;
+    private Method getMobileDataEnabled , setMobileDataEnabled;
+    public boolean socketOpened = false;
+
+    public  DataSwitch(int portno,ConnectivityManager connectivityManagerInstance)  {
+
+
+        connectivityManager = connectivityManagerInstance;
+        portNumber = portno;
+
+
+        try {
+            serverSocket = new ServerSocket(portNumber);
+            Class connectivityManagerClass = connectivityManager.getClass();
+            getMobileDataEnabled = connectivityManagerClass.getMethod("getMobileDataEnabled");
+            setMobileDataEnabled = connectivityManagerClass.getMethod("setMobileDataEnabled",boolean.class);
+            Log.e("Server :","Started");
+            socketOpened = true;
+        }
+        catch(Exception e)
+        {
+            socketOpened = false;
+            Log.e("Exception Message :",e.getMessage());
+        }
+    }
+    public void run()
+    {
+        while(true)
+        {
+            try (
+
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out =
+                            new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+            ) {
+
+                String inputLine, outputLine;
+
+                // Initiate conversation with client
+                if(!(boolean)getMobileDataEnabled.invoke(connectivityManager))
+                    out.println("off");
+                else
+                out.println("on");
+
+
+                while ((inputLine = in.readLine()) != null) {
+
+                    if (inputLine.equals("on"))
+                    {
+
+
+                        if(!(boolean)getMobileDataEnabled.invoke(connectivityManager)) {
+
+                            setMobileDataEnabled.invoke(connectivityManager,true);
+                            out.println("on");
+                        }
+                    }
+                    else if(inputLine.equals("off"))
+                    {
+
+                        if((boolean)getMobileDataEnabled.invoke(connectivityManager)) {
+
+                            setMobileDataEnabled.invoke(connectivityManager,false);
+                            out.println("off");
+                        }
+                    }
+
+                }
+            } catch (IOException e) {
+                Log.e("Connection Message :","Exception caught when trying to listen on port "
+                        + portNumber + " or listening for a connection");
+                Log.e("Exception Message :",e.getMessage());
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
---
+++ java/com/android/dataswitch/MyService.java
@@ -0,0 +1,45 @@
+package com.android.dataswitch;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DataSwitch ds = new DataSwitch(2500,(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE));
+        if(ds.socketOpened)
+        ds.start();
+
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentTitle("DataSwitch").build();
+
+            startForeground(1, notification);
+
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopForeground(true);
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
